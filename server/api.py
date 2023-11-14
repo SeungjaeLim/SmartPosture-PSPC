a74dab7baad025fd860eb7291ce450c9fc1a864b
Seungjae Lim <seungjaelim@kaist.ac.kr>
2023-11-14 16:43:24 +0900

    CHORE: Setup backend

---
+++ server/api.py
@@ -0,0 +1,13 @@
+# api.py
+
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/items/")
+async def read_items():
+    return [{"name": "Item 1"}, {"name": "Item 2"}]
+
+@router.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"name": f"Item {item_id}"}
---
+++ server/main.py
@@ -0,0 +1,12 @@
+# main.py
+
+from fastapi import FastAPI
+from api import router as api_router
+
+app = FastAPI()
+
+app.include_router(api_router)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
